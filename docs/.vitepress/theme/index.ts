134d8c64232227af1e9370556c3227a4d431ae77
zhang lei <zhanglei@loklok.com>
2026-01-06 17:04:12 +0800

    init

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import Layout from './Layout.vue'
 import './style.css'
 import CusdisComment from './components/CusdisComment.vue'
 
